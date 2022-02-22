709a8e1554b9c53d48d69e47a51a12e2cfcf596d
unknown <fh4itzbr@gmail.com>
2022-02-22 12:29:35 -0300

    first Commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Frontend Mentor - Intro component with sign up form solution
+
+This is a solution to the [Intro component with sign up form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/intro-component-with-signup-form-5cf91bd49edda32581d28fd1). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Overview
+
+### The challenges
+
+Users should be able to:
+
+- View the optimal layout depending on their device's screen size
+### Links
+
+- Solution URL: (https://felonta.github.io/Intro-Sign-Up/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+
+
+```
+
+## Author
+
+- Website - Felonta - Gabriel
+- Frontend Mentor - [@felonta](https://www.frontendmentor.io/profile/felonta)
---
+++ js/script.js
@@ -0,0 +1,28 @@
+// const form = document.getElementById("form");
+// const fname = document.getElementById("fname");
+// const lname = document.getElementById("lname");
+// const email = document.getElementById("email");
+// const password = document.getElementById("password");
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   var firstName = fname.value.trim();
+//   var lastName = lname.value.trim();
+//   var emailValue = email.value.trim();
+//   var passwordValue = password.value.trim();
+
+//   if (firstName === "") {
+//     errorFunc(fname, "First Name Cannot be empty");
+//   } else {
+//     sucessfunc(fname);
+//   }
+// });
+
+// function errorFunc(req, message) {
+//   const formControl = req.parentElement;
+//   const span = formControl.querySelector("span");
+//   span.innetText = message;
+//   req.className += "error-text";
+//   span.className += "error";
+// }
